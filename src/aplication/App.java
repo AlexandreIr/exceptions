ad82f9a81346bfb3fab64cae33d9a99691441b9a
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-04-01 23:01:10 -0300

    feat: very bad solution on exception handle

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -25,7 +25,26 @@ public class App {
 		} else {
 			Reservation reservation = new Reservation(roomNumb, checkin, checkout);
 			System.out.println("Reservation: "+reservation);
-			System.out.println("Nights: "+reservation.duration());
+			
+			System.out.println();
+			System.out.println("Enter data to update reservation ");
+			System.out.print("Check-in (dd/mm/yyyy): ");
+			checkin = LocalDate.parse(sc.next(),formatter);
+			
+			System.out.print("Check-out (dd/mm/yyyy): ");
+			checkout = LocalDate.parse(sc.next(), formatter);
+			
+			LocalDate now = LocalDate.now();
+			
+			if(checkin.isBefore(now) || checkout.isBefore(now)) {
+				System.out.println("Error: check-in or check-out before now");
+			} else if(!checkout.isAfter(checkin)) {
+				System.out.println("Error in reservation check-out before check-in");
+			} else {
+				reservation.updateDates(checkin, checkout);
+				System.out.println("Reservation: "+reservation);				
+			}
+			
 		}
 		
 		
